8f092f19e30f555695115757ad7bac1cd6837bd7
Gongfan Fang <fgf@zju.edu.cn>
2021-11-02 14:36:37 +0800

    #57: fix an import error for load_state_dict_from_url

--- network/backbone/resnet.py
+++ network/backbone/resnet.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+try: # for torchvision<0.4
+    from torchvision.models.utils import load_state_dict_from_url
+except: # for torchvision>=0.4
+    from torch.hub import load_state_dict_from_url
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
