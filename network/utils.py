66235595f62cce864c9280a980fe927da23cc7dd
Timothy Lim <timothylimyonglee@gmail.com>
2021-11-16 17:47:14 +0800

    changing intermediate layers editor to include the usage of hrnetv2 backbone

--- network/utils.py
+++ network/utils.py
@@ -49,9 +49,11 @@ class IntermediateLayerGetter(nn.ModuleDict):
         >>>     [('feat1', torch.Size([1, 64, 56, 56])),
         >>>      ('feat2', torch.Size([1, 256, 14, 14]))]
     """
-    def __init__(self, model, return_layers):
+    def __init__(self, model, return_layers, hrnet_flag=False):
         if not set(return_layers).issubset([name for name, _ in model.named_children()]):
             raise ValueError("return_layers are not present in model")
+
+        self.hrnet_flag = hrnet_flag
 
         orig_return_layers = return_layers
         return_layers = {k: v for k, v in return_layers.items()}
@@ -69,8 +71,23 @@ class IntermediateLayerGetter(nn.ModuleDict):
     def forward(self, x):
         out = OrderedDict()
         for name, module in self.named_children():
-            x = module(x)
+            if self.hrnet_flag and name.startswith('transition'): # if using hrnet, you need to take care of transition
+                if name == 'transition1': # in transition1, you need to split the module to two streams first
+                    x = [trans(x) for trans in module]
+                else: # all other transition is just an extra one stream split
+                    x.append(module(x[-1]))
+            else: # other models (ex:resnet,mobilenet) are convolutions in series.
+                x = module(x)
+
             if name in self.return_layers:
                 out_name = self.return_layers[name]
-                out[out_name] = x
+                if name == 'stage4' and self.hrnet_flag: # In HRNetV2, we upsample and concat all outputs streams together
+                    output_h, output_w = x[0].size(2), x[0].size(3)  # Upsample to size of highest resolution stream
+                    x1 = F.interpolate(x[1], size=(output_h, output_w), mode='bilinear', align_corners=False)
+                    x2 = F.interpolate(x[2], size=(output_h, output_w), mode='bilinear', align_corners=False)
+                    x3 = F.interpolate(x[3], size=(output_h, output_w), mode='bilinear', align_corners=False)
+                    x = torch.cat([x[0], x1, x2, x3], dim=1)
+                    out[out_name] = x
+                else:
+                    out[out_name] = x
         return out
