d5d779a413b10b70113be22df7d7dbe3aa25edc6
ladamoroz <laduskamorozova@gmail.com>
2022-09-13 17:50:53 +0300

    Improvment of postprocessing

--- predict.py
+++ predict.py
@@ -86,7 +86,7 @@ def main():
         image_files.append(opts.input)
     
     # Set up model (all models are 'constructed at network.modeling)
-    model = network.modeling.__dict__[opts.model](num_classes=opts.num_classes, output_stride=opts.output_stride)
+    model = network.modeling.__dict__[opts.model](num_classes=opts.num_classes, output_stride=opts.output_stride, pretrained_backbone=False)
     if opts.separable_conv and 'plus' in opts.model:
         network.convert_to_separable_conv(model.classifier)
     utils.set_bn_momentum(model.backbone, momentum=0.01)
@@ -109,8 +109,8 @@ def main():
 
     if opts.crop_val:
         transform = T.Compose([
-                T.Resize(opts.crop_size),
-                T.CenterCrop(opts.crop_size),
+                T.Resize((opts.crop_size, opts.crop_size)),
+                # T.CenterCrop(opts.crop_size),
                 T.ToTensor(),
                 T.Normalize(mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225]),
@@ -129,15 +129,19 @@ def main():
         for img_path in tqdm(image_files):
             ext = os.path.basename(img_path).split('.')[-1]
             img_name = os.path.basename(img_path)[:-len(ext)-1]
+            camera_angle = np.array(Image.open(f"ignore_masks/{img_name.split('_')[0]}.png"))
             img = Image.open(img_path).convert('RGB')
             img = transform(img).unsqueeze(0) # To tensor of NCHW
             img = img.to(device)
-            
             pred = model(img).max(1)[1].cpu().numpy()[0] # HW
-            colorized_preds = decode_fn(pred).astype('uint8')
-            colorized_preds = Image.fromarray(colorized_preds)
+            colorized_preds= decode_fn(pred).astype('uint8')
+            tmp_preds =colorized_preds
+            tmp_preds = np.array(T.Resize(camera_angle.shape, interpolation=T.InterpolationMode.NEAREST)(Image.fromarray(colorized_preds)))
+            tmp_preds[camera_angle == 255] = 255
+            colorized_preds = Image.fromarray(tmp_preds)
             if opts.save_val_results_to:
                 colorized_preds.save(os.path.join(opts.save_val_results_to, img_name+'.png'))
 
 if __name__ == '__main__':
     main()
+
